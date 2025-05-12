727b5f8af62865f6200cfc6ad5a8ac6ba590c3c8
Aldo Zunino <zunino.aldo@gmail.com>
2025-05-11 22:17:44 -0300

    v4.6

--- src/app/Features/auth/login/login.component.spec.ts
+++ src/app/Features/auth/login/login.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 
+
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
@@ -19,5 +20,34 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should start with email and password empty values', () => 
+  expect(component.loginForm.value).toEqual({
+    email: '',
+    password: ''
+  }));
+
+  it('should start with login button disabled', () => 
+  expect(component.loginForm.controls['login'].disabled).toBeTrue());
+
+  it('should mark email as invalid when it is empty', () => 
+  expect(component.loginForm.controls['email'].valid).toBeFalse());
+
+  it('should mark password as invalid when it is empty', () => 
+  expect(component.loginForm.controls['password'].valid).toBeFalse());
+
+  it('should mark the form as invalid when password is less than 4 characters', () => {
+    component.loginForm.controls['password'].setValue('123');
+    expect(component.loginForm.valid).toBeFalse();
+  });
+
+  it('should call authService.login when the form is valid', () => {
+    const AuthServiceSpy = spyOn(component['AuthService'], 'login').and.returnValue(true);
+    const routerSpy = spyOn(component['router'], 'navigate');
+    component.loginForm.setValue({email: 'admin@admin.com', password: 'admin'});
+    component.submit();
+    expect(AuthServiceSpy).toHaveBeenCalledWith('admin@admin.com', 'admin');
+    expect(routerSpy).toHaveBeenCalledWith(['/dashboard']);
   });
 });
